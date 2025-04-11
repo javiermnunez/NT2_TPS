07153ace1ace97533f6ead5576dc4be2b1352b61
javiermnunez <javier.nuez@outlook.es>
2025-04-10 21:50:57 -0300

    agregue modulo para productos

--- TP1/parte01.js
+++ TP1/parte01.js
@@ -1,12 +1,6 @@
 
 //Parte 1
-const productos = [
-    { id: 1, nombre: "Remera", precio: 1500, categoria: "Ropa" },
-    { id: 2, nombre: "Pantalón", precio: 2500, categoria: "Ropa" },
-    { id: 3, nombre: "Zapatillas", precio: 8000, categoria: "Calzado" },
-    { id: 4, nombre: "Gorra", precio: 1200, categoria: "Accesorios" },
-    { id: 5, nombre: "Campera", precio: 9500, categoria: "Ropa" }
-    ];
+import productos from './productos.js';
     
 console.log(productos[0]["nombre"])
 
---
+++ TP1/productos.js
@@ -0,0 +1,8 @@
+const productos = [
+    { id: 1, nombre: "Remera", precio: 1500, categoria: "Ropa" },
+    { id: 2, nombre: "Pantalón", precio: 2500, categoria: "Ropa" },
+    { id: 3, nombre: "Zapatillas", precio: 8000, categoria: "Calzado" },
+    { id: 4, nombre: "Gorra", precio: 1200, categoria: "Accesorios" },
+    { id: 5, nombre: "Campera", precio: 9500, categoria: "Ropa" }
+  ];
+export default productos;
